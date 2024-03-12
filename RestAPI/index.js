fb96bd3a282ddbcc1c781a5f8126718087765f55
Divyanshi-Varde <vardedinky1702@gmail.com>
2024-03-12 17:04:48 +0530

    Created A REST API

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -4,6 +4,7 @@ const users = require("./MOCK_DATA.json");
 const PORT = 8080;
 
 const app = express();
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -26,18 +27,35 @@ app
     const user = users.find((user) => user.id === id);
     return res.json(user);
   })
-  .put((req, res) => {
-    return res.json({ status: "pending" });
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const body = req.body;
+    const user = users.find((user) => user.id === id);
+    const updatedUser = { ...user, ...body };
+    updatedUser.id = id;
+    users[id - 1] = updatedUser;
+
+    fs.writeFile("MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "Success", updatedUser });
+    });
   })
   .delete((req, res) => {
-    return res.json({ status: "pending" });
+    const id = Number(req.params.id);
+    const user = users.findIndex((user) => user.id === id);
+
+    if (user !== -1) {
+      const deletedUser = users.splice(user, 1)[0];
+      fs.writeFile("MOCK_DATA.json", JSON.stringify(users), (err, data) =>
+        res.json({ status: "Success", deletedUser })
+      );
+    }
   });
 
 app.post("/api/users", (req, res) => {
   const body = req.body;
-  users.push({ id: users.length + 1, ...body });
+  users.push({ id: users.length, ...body });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "pending" });
+    return res.json({ status: "success", id: users.length + 1 });
   });
 });
 
